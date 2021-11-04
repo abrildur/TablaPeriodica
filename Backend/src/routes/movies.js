e53fefe030b6e1e98d281ab1988b2583a0085277
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-11-04 06:28:19 +0000

    se modifico la ruta de movies

--- Backend/src/routes/movies.js
+++ Backend/src/routes/movies.js
@@ -4,8 +4,13 @@ const router = Router();
 const movies = require('./sample.json');
 console.log(movies);
 
-router.get('/movies', (req, res) => {
+router.get('/', (req, res) => {
     res.send('movies');
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('received');
 });
 
 module.exports = router;
