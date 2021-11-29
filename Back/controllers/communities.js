9e7e43e4a8240672f0462d9cc92db575a44052e6
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-11-29 18:35:02 +0000

    se modifico el archivo de communities

---
+++ Back/controllers/communities.js
@@ -0,0 +1,15 @@
+let communities = [];
+
+exports.createCommunities = async(req, res) =>{
+    /*
+    console.log();
+    setTimeout(()=>{}, 500)
+    console.log('algo')
+    */
+   try {
+       const { body } = req;
+
+   } catch (error) {
+      
+   }
+}
