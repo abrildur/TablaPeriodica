25400d1568d40b709b374dadbc5479edc3048cc7
Abril Daniela <71578178+abrildur@users.noreply.github.com>
2021-11-29 05:49:20 +0000

    cambios

--- Back/index.js
+++ Back/index.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express
+const app = express();
+
+//middlewares 
+app.use(morgan('dev'));
+
+app.use(express.json({ limit: '50mb'}));
+
+/**methodos http 
+ * post: agregar info
+ * get : alistar info
+ * put : actualizar
+ * delete : eliminar
+ * similar a -> CRUD BD */
+
+//routes 
+
+app.get('/', (req, res)=>{
+    res.json({message:'bienvenido a nuestro primer navegador'});
+});
+// statics files 
+
+//start
+
+app.listen(3000, ()=> {
+    console.log("Servidor esta corriendo en el puerto: ", 3000);
+});
+
